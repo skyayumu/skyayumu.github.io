b3f7306997e4fc4f601eeb29d98830e42c1e313a
skyayumu <e2358119@soka-u.jp>
2024-07-31 22:35:11 +0900

    最終課題の修正

--- js/main.js
+++ js/main.js
@@ -1,60 +1,104 @@
-// マウスに足跡をつける
-let lastX = 0;
-let lastY = 0;
-const distanceThreshold = 30; // 表示するためのマウス移動の距離
+// 条件分岐（if ... else）
 
-document.addEventListener('mousemove', function (event) {
-    const deltaX = event.pageX - lastX;
-    const deltaY = event.pageY - lastY;
-    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+let condition = 5;
+// let condition = -3;
+// let condition = 0;
+// let condition = "0";
 
-    if (distance > distanceThreshold) {
-        // マウスの動きの方向を基準に角度を計算
-        const angle = Math.atan2(deltaY, deltaX) * 180 / Math.PI;
+// if ... elseは、上から順番に条件式が評価され、trueとなった節以降の条件は無視される
+if (condition > 0){
+    console.log("condetionは、正の数です");
+}
+else if (condition === 0){
+    console.log("condetionは、0です");
+}
+else if (condition < 0){
+    console.log("condetionは、負の数です");
+}
+else{
+    console.log("condetionに格納されている値は、数字ではありません");
+}
 
-        let footpoint = document.createElement('div');
-        footpoint.className = 'footpoint';
-        footpoint.style.left = event.pageX + 'px';
-        footpoint.style.top = event.pageY + 'px';
-        footpoint.style.transform = `rotate(${angle + 90}deg)`; // 角度に90度を追加
-        footpoint.style.opacity = '1'; // 足跡マークを可視化
-        document.body.appendChild(footpoint);
+// 引数で指定された数字によって、いろいろな言語で「こんにちは」を表示する関数
+function hello(language){
+    // 表示する文字列を格納する変数
+    let message = "";
 
-        setTimeout(function () {
-            footpoint.remove();
-        }, 250); // 足跡マークを0.25秒後に削除します
-
-        lastX = event.pageX;
-        lastY = event.pageY;
+    // languageに入っている値で、（１：日本語、２：英語、３：中国語、４：韓国語）
+    if (language === 1){
+        message = "こんにちは";
     }
-});
+    else if (language === 2){
+        message = "Hello";
+    }
+    else if (language === 3){
+        message = "你好";
+    }
+    else if (language === 4){
+        message = "안녕하세요";
+    }
+    else{
+        message = "想定されていない引数です";
+    }
 
-// homeに最初に足跡マークを出す
-document.querySelectorAll('.footprint').forEach((footprint, index) => {
-    footprint.addEventListener('animationend', function handleAnimationEnd() {
-        // アニメーションが終了した後に要素を削除
-        setTimeout(() => {
-            this.remove();
-        }, 10000); // インデックスに応じた遅延を追加することも可能
-        // イベントリスナーを削除してメモリリークを防ぐ
-        this.removeEventListener('animationend', handleAnimationEnd);
-    });
-});
+    //  コンソールに表示
+    console.log(message);
 
-// homeに最初にYOARIKIを表示する
-const textContainer = document.getElementById('text-container');
-const text = textContainer.innerText;
-textContainer.innerHTML = ''; // 元のテキストをクリア
+    // HTMLを書き換えたい場合
+    // HTMLの要素を取得する
+    const p_hello = document.querySelector("#hello");
+    console.log(p_hello);
 
-text.split('').forEach((char, index) => {
-    const span = document.createElement('span');
-    span.classList.add('char');
-    span.style.animationDelay = `${0.65 + index * 0.35}s`; // 0.1秒ごとに遅延
-    span.textContent = char;
-    textContainer.appendChild(span);
-});
+    // 取得したHTML要素の文字列を置き換える
+    p_hello.textContent = message;
 
-function toggleMenu() {
-    const navMenu = document.querySelector('.nav-menu');
-    navMenu.classList.toggle('active');
+}
+
+// 関数のテスト
+// hello(1);
+
+// 繰り返し（while）
+// ループに複雑な条件処理を組み合わせる場合に使うことが多い
+let count = 0;
+while (count < 5){
+    console.log("while文のテスト：" + count + "回目のループです")
+
+    // 途中でループ空けいた場合は、break文を使う
+    if (count === 3){
+        break;
+    }
+
+    // count = count + 1;
+    count++;
+}
+
+// 繰り返し（for）
+// 事前にループの回数が分かっている場合や配列と組み合わせる場合に使うことが多い。
+for (let i = 0; i < 5; i++){
+    console.log("for文のテスト：" + i + "回目のループです");
+}
+
+let player_hand = ["グー", "チョキ", "パー"];
+// console.log(player_hand[0]);
+
+for (let i = 0; i < 5; i++){
+    console.log(player_hand[i]);
+}
+
+// 配列名.lengthで配列の要素数（長さ）を取得できる
+console.log(player_hand.length)
+
+// 配列の長さに合わせて自動ループ
+for(let i = 0; i < player_hand.length; i++){
+    console.log(player_hand[i]);
+}
+
+//  さらにスマートに
+for (const hand of player_hand){
+    console.log("じゃんけん：" + hand);
+}
+
+let aiueo = "じゃんけん";
+for (const str of aiueo){
+    console.log("じゃんけん：" + str);
 }
